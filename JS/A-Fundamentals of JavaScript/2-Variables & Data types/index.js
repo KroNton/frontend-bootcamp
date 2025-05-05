4a2b8901911ad69332b69356a636d69e86693264
KroNton <kyrlosfekry@gmail.com>
2025-05-06 02:53:02 +0300

    Add HTML and JavaScript files for Variables & Data Types section

---
+++ JS/A-Fundamentals of JavaScript/2-Variables & Data types/index.js
@@ -0,0 +1,18 @@
+let name = "kyrillos";
+let age = 22;
+let isStudent = true;
+let isLoggedIn = false;
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// null variable
+isLoggedIn = null;
+console.log(isLoggedIn);
+
+// undefined variable
+let welcomeMessage;
+console.log(welcomeMessage);
+
+welcomeMessage = "Welcome to the course";
+console.log(welcomeMessage);
