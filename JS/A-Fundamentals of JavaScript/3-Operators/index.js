ba827ea0cf780dd3e391d885e39848f50738202d
KroNton <kyrlosfekry@gmail.com>
2025-05-06 02:53:10 +0300

    Add HTML and JavaScript files for Operators section

---
+++ JS/A-Fundamentals of JavaScript/3-Operators/index.js
@@ -0,0 +1,23 @@
+
+let num1 = 22;
+let num2 = 22;
+let num3 = 18;
+
+console.log("the result is: " + (num1 + num2)); // 44
+
+let result = num1 * num2;
+
+console.log("the result is: " + result); // 484
+
+let isgreater = num1 > num2;
+
+console.log("is num1 greater than num2? " + isgreater); // false
+
+let isequal = num1 == num2;
+console.log("is num1 equal to num2 ? " + isequal); // true
+
+let check = ((num1 + num2) > num3) && (num3 < 20);
+
+console.log("is num1 + num2 greater than num3 and num3 less than 20 ? " + check); // true
+
+console.log(5 % 2); // 1
