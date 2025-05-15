9e3b06d81e0f450464c7d309582c035fe809d6fd
KroNton <kyrlosfekry@gmail.com>
2025-05-16 02:10:03 +0300

    Add initial HTML and JavaScript files for class creation example

---
+++ JS/D-Classes & OOP/1-How to write a class/index.js
@@ -0,0 +1,22 @@
+class Student {
+    // Properties
+    name;
+    age;
+    grade;
+
+    // Constructor
+    constructor(parameters) {
+
+    }
+    // Methods
+
+
+}
+// create an instance of the class (object)
+const s = new Student();
+
+s.name = "John";
+s.age = 20;
+s.grade = 90;
+console.log(s);
+console.log(s.name); // John
