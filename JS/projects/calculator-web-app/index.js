c34b6f53826c3d71fb6b0352f58a05decf17f83d
KroNton <kyrlosfekry@gmail.com>
2025-05-17 16:11:06 +0300

    Refactor calculator app to improve interaction handling and UI updates

--- JS/projects/calculator-web-app/index.js
+++ JS/projects/calculator-web-app/index.js
@@ -1,3 +1,54 @@
-console.log("Hello World!");
+
 // This is a simple calculator web app
-const displayInput = document.getElementById('inputValue').innerText;
+const displayInput = document.getElementById('inputValue');
+
+const operators = ['-', '+', '%', '*', '/']
+let operations = []
+let currentValue = ''
+
+function handleInteraction(value) {
+    console.log(value);
+    if (operators.includes(value)) {
+        // console.log('clicked an operator: ', value);
+        handleOperatorsInput(value)
+
+    } else {
+        // console.log('clicked a numeric value: ', value);
+        handleNumInput(value)
+
+    }
+    updateUI()
+}
+function handleNumInput(value) {
+    currentValue += value;
+    // console.log(' newvalue: ', currentValue);
+}
+function handleOperatorsInput(value) {
+    if (!currentValue) {
+        return
+    }
+
+    operations.push(currentValue)
+    operations.push(value)
+    currentValue = ''
+
+}
+
+function handleEvaluate() {
+    let finalAmount = operations.
+}
+
+function handleReset() {
+    currentValue = ''
+    operations = []
+    updateUI()
+
+}
+
+function updateUI() {
+    const displayString = operations.join(' ') + ' ' + currentValue
+    displayInput.innerText = displayString.trim() ? displayString : '0'
+
+    console.log(operations);
+}
+
