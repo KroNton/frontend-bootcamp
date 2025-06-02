bde5bf5f44c6b9e4f26591b5b2d9fbc3f21f5516
KroNton <kyrlosfekry@gmail.com>
2025-06-02 12:26:31 +0300

    Update temperature display to use Celsius and adjust weather fetching logic

--- JS/projects/weather-app/index.js
+++ JS/projects/weather-app/index.js
@@ -1,22 +1,31 @@
+// DOM Elements 
+const currentTemp = document.getElementById('current-temp')
+
 
 // API Configuration
 const apiKey = '1325e63c6ea5350145034a215a822516';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
 const units = 'metric'
 let city = 'cairo'
-async function checkWeather() {
-    let weatherData = null
-    const response = await fetch(baseUrl + `units=${units}` + `&q=${city}` + `&appid=${apiKey}`, {
-        method: "GET"
-    }).then((data) => {
+async function fetchWeather(city) {
+    const response = await fetch(baseUrl + `units=${units}&q=${city}&appid=${apiKey}`)
+    if (!response.ok) {
+        console.log("city not found")
 
-        return data.json();
-    }).then((data) => {
-        weatherData = data
-    })
-    return weatherData
+    }
+
+    const weatherData = await response.json();
+    return weatherData;
 }
 
-let weatherData = checkWeather()
 
-console.log(weatherData)
+async function desplayWeather() {
+
+    const weatherData = await fetchWeather(city);
+    currentTemp.textContent = weatherData.main.temp
+    console.log(weatherData.main.feels_like)
+}
+
+desplayWeather()
+
+functioupdate 
